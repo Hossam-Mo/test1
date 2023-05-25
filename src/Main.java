66366fc0cb3b434aae6798e595eb4d543c4d34f5
Hossam-mo <hossam.m.pro@gmail.com>
2023-05-25 15:59:55 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// https://github.com/Hossam-Mo/test1.git
+
+
+
+
+public class Main {
+
+
+
+
+
+            public static void main(String args[]){
+                Demo a = new Demo(2,3);
+                System.out.println(a.biggerNum());
+
+            }
+
+}
